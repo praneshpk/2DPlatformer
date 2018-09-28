1bd2d3f1d32567885f2572d21077976fbc0fabc5
Pranesh Kamalakanthan <pkamala@ncsu.edu>
2018-09-27 22:44:58 -0400

    client-server chat basis made

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,10 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -10,15 +12,13 @@ public class Server {
 
     public static int PORT = 4096;
     public static String HOSTNAME = "127.0.0.1";
+    public static int MAX_USERS = 3;
 
     private ServerSocket server;
 
-    private static class User {
-        String name;
-        int id;
-    }
-
-    private User[] users;
+    private ArrayList<String> users = new ArrayList<>();
+    private static String log = "";
+    private static Object lock = new Object();
 
     /**
      Class to allow multithreading across users
@@ -29,35 +29,51 @@ public class Server {
             this.s = s;
         }
         public void run() {
-            handleClient(s);
+            try {
+                handleClient(s);
+            } catch (Exception e) {
+            }
         }
     }
 
-    public void handleClient(Socket s) {
-        String client = null;
+    public void handleClient(Socket s) throws IOException {
+        String client = "";
+        InetAddress ip = s.getLocalAddress();
+        DataOutputStream output = new DataOutputStream(s.getOutputStream());
+        DataInputStream input = new DataInputStream(s.getInputStream());
+        boolean fin = true;
         try {
-            DataOutputStream output = new DataOutputStream(s.getOutputStream());
-            DataInputStream input = new DataInputStream(s.getInputStream());
-
             client = input.readUTF();
-            System.out.println(client + "/" + s.getLocalAddress() + " joined.");
-            output.writeUTF(client + " has joined the server.\n");
-
-            String log = input.readUTF();
-            while(!log.equals(":Q")) {
+            if(users.size() == MAX_USERS)
+                throw new IllegalStateException();
+            while(users.contains(client)) {
+                output.writeUTF("err");
+                client = input.readUTF();
+            }
+            users.add(client);
+            System.out.println(client + ip + " joined.");
+            log += client + " has joined the server.\n";
+            output.writeUTF(log);
+            String request = input.readUTF();
+            while(true) {
+                synchronized ( lock ) {
+                    log += request + "\n";
+                    lock.notifyAll();
+                }
                 output.writeUTF(log);
-                log = input.readUTF();
+                request = input.readUTF();
             }
-            // block
-        } catch (Exception e) {
-            // to-do
+        } catch (IllegalStateException e) {
+            output.writeUTF("Error: Server is full!");
+            s.close();
+            fin = false;
         } finally {
-            System.out.println(client + "/" + s.getLocalAddress() + " left.");
-            try {
+            if(fin) {
+                System.out.println(client + ip + " left.");
+                users.remove(client);
                 s.close();
-            } catch(Exception e) {
-
             }
+
         }
 
     }
@@ -76,7 +92,6 @@ public class Server {
         try {
             while(true) {
                 Socket s = server.accept();
-
                 UserThread t = new UserThread(s);
                 t.start();
             }
